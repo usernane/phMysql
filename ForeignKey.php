94d1ba6d8ef9a9e801a9cf48be7bfe2ac812c577
Ibrahim <ibinshikh@hotmail.com>
2018-06-26 09:04:54 +0300

    Allowing numbers in key name

--- ForeignKey.php
+++ ForeignKey.php
@@ -95,7 +95,7 @@ class ForeignKey {
             if(strpos($name, ' ') === FALSE){
                 for ($x = 0 ; $x < strlen($name) ; $x++){
                     $ch = $name[$x];
-                    if($ch == '_' || ($ch >= 'a' && $ch <= 'z') || ($ch >= 'A' && $ch <= 'Z')){
+                    if($ch == '_' || ($ch >= 'a' && $ch <= 'z') || ($ch >= 'A' && $ch <= 'Z') || ($ch >= '0' && $ch <= '9')){
 
                     }
                     else{
