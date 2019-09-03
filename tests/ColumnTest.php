d2cd803e5d3a57b69219660de20ed3f734f5a64c
Ibrahim BinAlshikh <usernane@users.noreply.github.com>
2019-09-04 01:33:57 +0300

    Update ColumnTest.php

--- tests/ColumnTest.php
+++ tests/ColumnTest.php
@@ -83,6 +83,40 @@ class ColumnTest extends TestCase{
     /**
      * @test
      */
+    public function testConstructor08() {
+        $col = new Column('valid_name', 'Varchar ', 0);
+        $this->assertEquals('varchar',$col->getType());
+        $this->assertEquals(1,$col->getSize());
+        $this->assertEquals('valid_name',$col->getName());
+    }
+    /**
+     * @test
+     */
+    public function testConstructor09() {
+        $col = new Column('amount', 'decimal ');
+        $this->assertEquals('decimal',$col->getType());
+        $this->assertEquals(0,$col->getSize());
+        return $col;
+    }
+    /**
+     * 
+     * @param Column $col
+     * @depends testConstructor09
+     */
+    public function testSetScale00($col) {
+        $col->setSize(10);
+        $this->assertTrue($col->setScale(3));
+        $this->assertEquals(3,$col->getScale());
+        $this->assertTrue($col->setScale(0));
+        $this->assertEquals(0,$col->getScale());
+        $this->assertTrue($col->setScale(9));
+        $this->assertEquals(9,$col->getScale());
+        $this->assertFalse($col->setScale(10));
+        $this->assertEquals(9,$col->getScale());
+    }
+    /**
+     * @test
+     */
     public function testSetColName00() {
         $col = new Column();
         $this->assertTrue($col->setName('my_file'));
