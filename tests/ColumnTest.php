cd5f1eb9fac121e5cc9ca9c382e45d2bc165897f
Ibrahim BinAlshikh <usernane@users.noreply.github.com>
2019-09-03 17:20:53 +0300

    Update ColumnTest.php

--- tests/ColumnTest.php
+++ tests/ColumnTest.php
@@ -21,10 +21,64 @@ class ColumnTest extends TestCase{
      * @test
      */
     public function testConstructor01() {
-        $col = new Column('user_id', 'varchar', 15);
+        $col = new Column('user_id ', 'varchar', 15);
         $this->assertEquals('varchar',$col->getType());
         $this->assertEquals(15,$col->getSize());
         $this->assertEquals('user_id',$col->getName());
+    }
+    /**
+     * @test
+     */
+    public function testConstructor02() {
+        $col = new Column('invalid name', 'varchar', 15);
+        $this->assertEquals('varchar',$col->getType());
+        $this->assertEquals(15,$col->getSize());
+        $this->assertEquals('col',$col->getName());
+    }
+    /**
+     * @test
+     */
+    public function testConstructor03() {
+        $col = new Column('0invalid_name', 'varchar', 15);
+        $this->assertEquals('varchar',$col->getType());
+        $this->assertEquals(15,$col->getSize());
+        $this->assertEquals('col',$col->getName());
+    }
+    /**
+     * @test
+     */
+    public function testConstructor04() {
+        $col = new Column('valid_name', 'invalid type', 15);
+        $this->assertEquals('varchar',$col->getType());
+        $this->assertEquals(15,$col->getSize());
+        $this->assertEquals('valid_name',$col->getName());
+    }
+    /**
+     * @test
+     */
+    public function testConstructor05() {
+        $col = new Column('valid_name', 'InT', 15);
+        $this->assertEquals('int',$col->getType());
+        $this->assertEquals(11,$col->getSize());
+        $this->assertEquals('valid_name',$col->getName());
+    }
+    /**
+     * @test
+     */
+    public function testConstructor06() {
+        $col = new Column('valid_name', 'Varchar ', 15);
+        $this->assertEquals('varchar',$col->getType());
+        $this->assertEquals(15,$col->getSize());
+        $this->assertEquals('valid_name',$col->getName());
+    }
+    /**
+     * @test
+     */
+    public function testConstructor07() {
+        $col = new Column('valid_name', 'Varchar ', 21846);
+        $this->assertEquals('mediumtext',$col->getType());
+        $this->assertEquals(21846,$col->getSize());
+        $this->assertEquals('valid_name',$col->getName());
     }
     /**
      * @test
