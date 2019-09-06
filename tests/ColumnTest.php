d5539cfc88f562116b48d5d58d74c4e59b8b42d8
Ibrahim BinAlshikh <usernane@users.noreply.github.com>
2019-09-06 21:21:32 +0300

    Update ColumnTest.php

--- tests/ColumnTest.php
+++ tests/ColumnTest.php
@@ -123,5 +123,126 @@ class ColumnTest extends TestCase{
         $this->assertEquals('my_file',$col->getName());
         $this->assertTrue($col->setName('   user_id   '));
         $this->assertEquals('user_id',$col->getName());
+        $this->assertFalse($col->setName('0user_id   '));
+        $this->assertFalse($col->setName('use id   '));
+        $this->assertFalse($col->setName('  '));
+    }
+    /**
+     * @test
+     */
+    public function setCommentTest00() {
+        $col = new Column('user_id ', 'varchar', 15);
+        $col->setComment('A unique ID for the user.');
+        $this->assertEquals('A unique ID for the user.',$col->getComment());
+        $this->assertEquals('user_id varchar(15) not null collate utf8mb4_unicode_ci comment \'A unique ID for the user.\'',$col.'');
+        return $col;
+    }
+    /**
+     * @test
+     * @depends setCommentTest00
+     * @param Column $col Description
+     */
+    public function setCommentTest01($col) {
+        $col->setComment(null);
+        $this->assertNull($col->getComment());
+        $this->assertEquals('user_id varchar(15) not null collate utf8mb4_unicode_ci',$col.'');
+    }
+    /**
+     * @test
+     */
+    public function testSetDefault00() {
+        $col = new Column('date', 'timestamp');
+        $col->setDefault();
+        $this->assertEquals('current_timestamp',$col->getDefault());
+        $this->assertEquals('date timestamp not null default current_timestamp',$col.'');
+    }
+    /**
+     * @test
+     */
+    public function testSetDefault01() {
+        $col = new Column('date', 'timestamp');
+        $col->setDefault('2019-07-07 09:09:09');
+        $this->assertEquals('2019-07-07 09:09:09',$col->getDefault());
+        $this->assertEquals('date timestamp not null default \'2019-07-07 09:09:09\'',$col.'');
+    }
+    /**
+     * @test
+     */
+    public function testSetDefault02() {
+        $col = new Column('date', 'datetime');
+        $col->setDefault();
+        $this->assertEquals('now()',$col->getDefault());
+        $this->assertEquals('date datetime not null default now()',$col.'');
+    }
+    /**
+     * @test
+     */
+    public function testSetDefault03() {
+        $col = new Column('date', 'datetime');
+        $col->setDefault('2019-07-07 09:09:09');
+        $this->assertEquals('2019-07-07 09:09:09',$col->getDefault());
+        $this->assertEquals('date datetime not null default \'2019-07-07 09:09:09\'',$col.'');
+    }
+    /**
+     * @test
+     */
+    public function testSetDefault04() {
+        $col = new Column('date', 'datetime');
+        $col->setDefault('2019-15-07 09:09:09');
+        $this->assertNull($col->getDefault());
+        $this->assertEquals('date datetime not null',$col.'');
+        $col->setDefault('2019-12-33 09:09:09');
+        $this->assertNull($col->getDefault());
+        $this->assertEquals('date datetime not null',$col.'');
+        $col->setDefault('2019-12-31 24:09:09');
+        $this->assertNull($col->getDefault());
+        $col->setDefault('2019-12-31 23:60:09');
+        $this->assertNull($col->getDefault());
+        $col->setDefault('2019-12-31 23:59:60');
+        $this->assertNull($col->getDefault());
+        $col->setDefault('2019-12-31 23:59:59');
+        $this->assertEquals('2019-12-31 23:59:59',$col->getDefault());
+    }
+    /**
+     * @test
+     */
+    public function testSetDefault05() {
+        $col = new Column('id', 'int');
+        $this->assertEquals('id int(1) not null',$col.'');
+        $this->assertTrue($col->setDefault(-122));
+        $this->assertEquals(-122,$col->getDefault());
+        $this->assertEquals('id int(1) not null default -122',$col.'');
+        $this->assertFalse($col->setDefault(null));
+        $this->assertFalse($col->setDefault('a string'));
+        $this->assertFalse($col->setDefault(1.8));
+    }
+    /**
+     * @test
+     */
+    public function testSetDefault06() {
+        $col = new Column('id', 'varchar');
+        $this->assertEquals('id varchar(1) not null collate utf8mb4_unicode_ci',$col.'');
+        $this->assertTrue($col->setDefault('A random string.'));
+        $this->assertEquals('A random string.',$col->getDefault());
+        $this->assertEquals('id varchar(1) not null collate utf8mb4_unicode_ci default \'A random string.\'',$col.'');
+        $this->assertFalse($col->setDefault(null));
+        $this->assertFalse($col->setDefault(33));
+        $this->assertFalse($col->setDefault(1.8));
+    }
+    /**
+     * @test
+     */
+    public function testSetDefault07() {
+        $col = new Column('id', 'decimal');
+        $this->assertEquals('id decimal not null',$col.'');
+        $this->assertTrue($col->setDefault(1));
+        $this->assertEquals(1,$col->getDefault());
+        $this->assertEquals('id decimal not null default \'1\'',$col.'');
+        $this->assertTrue($col->setDefault(1.66));
+        $this->assertEquals(1.66,$col->getDefault());
+        $this->assertEquals('id decimal not null default \'1.66\'',$col.'');
+        $this->assertFalse($col->setDefault(null));
+        $this->assertFalse($col->setDefault('33'));
+        $this->assertFalse($col->setDefault(''));
     }
 }
