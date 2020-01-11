4caefc42e40e077b1aee282d67c78a96f2c4dccd
Ibrahim BinAlshikh <usernane@users.noreply.github.com>
2020-01-11 15:37:17 +0300

    Create JoinTest.php

---
+++ tests/JoinTest.php
@@ -0,0 +1,15 @@
+<?php
+namespace phMysql\tests;
+use PHPUnit\Framework\TestCase;
+use phMysql\tests\ArticleJoinUserQuery;
+/**
+ * Description of JoinTest
+ *
+ * @author Ibrahim
+ */
+class JoinTest extends TestCase{
+//    public function test00() {
+//        $query = new ArticleJoinUserQuery();
+//        $this->assertEquals('join_table',$query->getStructureName());
+//    }
+}
