e246584bc22668a4f432ec19bb1b5fe204724639
Ibrahim BinAlshikh <usernane@users.noreply.github.com>
2020-01-16 01:01:29 +0300

    Update JoinTest.php

--- tests/JoinTest.php
+++ tests/JoinTest.php
@@ -2,14 +2,18 @@
 namespace phMysql\tests;
 use PHPUnit\Framework\TestCase;
 use phMysql\tests\ArticleJoinUserQuery;
+use phMysql\tests\UsersQuery;
 /**
  * Description of JoinTest
  *
  * @author Ibrahim
  */
 class JoinTest extends TestCase{
-//    public function test00() {
-//        $query = new ArticleJoinUserQuery();
-//        $this->assertEquals('join_table',$query->getStructureName());
-//    }
+    public function test00() {
+        $articleQ = new ArticleQuery();
+        $userQ = new UsersQuery();
+        $joinQuery = $articleQ->join($userQ);
+        $joinQuery->select();
+        $this->assertEquals('select * from (select * from articles left join system_users) as T0;',$joinQuery->getQuery());
+    }
 }
