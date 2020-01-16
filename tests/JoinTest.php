0dd8b5f350fc7833f82b18771df4c4cd38be4bdb
Ibrahim BinAlshikh <usernane@users.noreply.github.com>
2020-01-17 02:56:11 +0300

    Update JoinTest.php

--- tests/JoinTest.php
+++ tests/JoinTest.php
@@ -9,11 +9,44 @@ use phMysql\tests\UsersQuery;
  * @author Ibrahim
  */
 class JoinTest extends TestCase{
+    /**
+     * @test
+     */
     public function test00() {
         $articleQ = new ArticleQuery();
         $userQ = new UsersQuery();
         $joinQuery = $articleQ->join($userQ);
         $joinQuery->select();
         $this->assertEquals('select * from (select * from articles left join system_users) as T0;',$joinQuery->getQuery());
+    }
+    /**
+     * @test
+     */
+    public function test01() {
+        $articleQ = new ArticleQuery();
+        $userQ = new UsersQuery();
+        $joinQuery = $articleQ->join($userQ);
+        foreach ($articleQ->getTable()->getColumns() as $index => $colObj){
+            $this->assertTrue($joinQuery->getTable()->hasColumn($index));
+        }
+        foreach ($userQ->getTable()->getColumns() as $index => $colObj){
+            $this->assertTrue($joinQuery->getTable()->hasColumn($index));
+        }
+    }
+    public function test02() {
+        $articleQ = new ArticleQuery();
+        $userQ = new UsersQuery();
+        $joinQuery = $articleQ->join($userQ);
+        $joinQuery->select([
+            'columns'=>[
+                'name','content'
+            ],
+            'where'=>[
+                'article-id'=>55
+            ],
+            'as-view'=>true,
+            'view-name'=>'author_content'
+        ]);
+        $this->assertEquals('','');
     }
 }
