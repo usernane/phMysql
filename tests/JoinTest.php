52b75b7f350e1e68867b23f22e0d86b66a1e29d7
Ibrahim BinAlshikh <usernane@users.noreply.github.com>
2020-01-20 21:18:35 +0300

    Update JoinTest.php

--- tests/JoinTest.php
+++ tests/JoinTest.php
@@ -15,9 +15,9 @@ class JoinTest extends TestCase{
     public function test00() {
         $articleQ = new ArticleQuery();
         $userQ = new UsersQuery();
-        $joinQuery = $articleQ->join($userQ);
+        $joinQuery = $articleQ->join($userQ,['author-id'=>'user-id']);
         $joinQuery->select();
-        $this->assertEquals('select * from (select * from articles left join system_users) as T0;',$joinQuery->getQuery());
+        $this->assertEquals('select * from (select * from articles left join system_users on articles.author_id = system_users.user_id) as T0;',$joinQuery->getQuery());
     }
     /**
      * @test
@@ -25,7 +25,7 @@ class JoinTest extends TestCase{
     public function test01() {
         $articleQ = new ArticleQuery();
         $userQ = new UsersQuery();
-        $joinQuery = $articleQ->join($userQ);
+        $joinQuery = $articleQ->join($userQ,['author-id'=>'user-id']);
         foreach ($articleQ->getTable()->getColumns() as $index => $colObj){
             $this->assertTrue($joinQuery->getTable()->hasColumn($index));
         }
@@ -36,7 +36,9 @@ class JoinTest extends TestCase{
     public function test02() {
         $articleQ = new ArticleQuery();
         $userQ = new UsersQuery();
-        $joinQuery = $articleQ->join($userQ);
+        $joinQuery = $articleQ->join($userQ,[
+            'author-id'=>'user-id'
+        ]);
         $joinQuery->select([
             'where'=>[
                 'article-id'=>55
@@ -44,6 +46,6 @@ class JoinTest extends TestCase{
             'as-view'=>true,
             'view-name'=>'author_content'
         ]);
-        $this->assertEquals('',$joinQuery->getQuery());
+        $this->assertEquals('create view author_content as (select * from (select * from articles left join system_users on articles.author_id = system_users.user_id) as T0 where T0.article_id = 55);',$joinQuery->getQuery());
     }
 }
