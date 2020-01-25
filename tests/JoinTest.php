48fe812f202f8749cdbba1a82cc4cd40da121c3c
Ibrahim BinAlshikh <usernane@users.noreply.github.com>
2020-01-25 03:12:32 +0300

    Update JoinTest.php

--- tests/JoinTest.php
+++ tests/JoinTest.php
@@ -15,7 +15,7 @@ class JoinTest extends TestCase{
     public function test00() {
         $articleQ = new ArticleQuery();
         $userQ = new UsersQuery();
-        $joinQuery = $articleQ->join($userQ,['author-id'=>'user-id']);
+        $joinQuery = $articleQ->join($userQ,['author-id'=>'user-id'],'left');
         $joinQuery->select();
         $this->assertEquals('select * from (select * from articles left join system_users on articles.author_id = system_users.user_id) as T0;',$joinQuery->getQuery());
     }
@@ -25,7 +25,7 @@ class JoinTest extends TestCase{
     public function test01() {
         $articleQ = new ArticleQuery();
         $userQ = new UsersQuery();
-        $joinQuery = $articleQ->join($userQ,['author-id'=>'user-id']);
+        $joinQuery = $articleQ->join($userQ,['author-id'=>'user-id'],'left');
         foreach ($articleQ->getTable()->getColumns() as $index => $colObj){
             $this->assertTrue($joinQuery->getTable()->hasColumn($index));
         }
@@ -33,12 +33,15 @@ class JoinTest extends TestCase{
             $this->assertTrue($joinQuery->getTable()->hasColumn($index));
         }
     }
+    /**
+     * @test
+     */
     public function test02() {
         $articleQ = new ArticleQuery();
         $userQ = new UsersQuery();
         $joinQuery = $articleQ->join($userQ,[
             'author-id'=>'user-id'
-        ]);
+        ],'left',[],[],'myT');
         $joinQuery->select([
             'where'=>[
                 'article-id'=>55
@@ -46,6 +49,6 @@ class JoinTest extends TestCase{
             'as-view'=>true,
             'view-name'=>'author_content'
         ]);
-        $this->assertEquals('create view author_content as (select * from (select * from articles left join system_users on articles.author_id = system_users.user_id) as T0 where T0.article_id = 55);',$joinQuery->getQuery());
+        $this->assertEquals('create view author_content as (select * from (select * from articles left join system_users on articles.author_id = system_users.user_id) as myT where myT.article_id = 55);',$joinQuery->getQuery());
     }
 }
